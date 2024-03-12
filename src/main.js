8ec03c3a1f224bf358ef501077f33ab254ebe571
Dung Dang Duc Bao <dangdungth.wa@gmail.com>
2024-03-13 00:09:25 +0700

    init

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import {logger} from './abc';
+
+createApp(App).mount('#app');
+
+logger();
+
+console.log(logger().canh);
